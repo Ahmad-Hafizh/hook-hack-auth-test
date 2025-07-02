a282b126bd75f267e46a9cd1af564b1b5ee1f2b6
langlangsatrio123 <satrio@samurai-style.tokyo>
2025-07-02 15:28:49 +0700

    public-middle-ware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/app(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth.protect();
-  }
-  // All other routes, including /api, are public and not protected
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
